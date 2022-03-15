cf462e0526825d789c0d712b6f10e63c794fbdab
unknown <mperkhounkov@gmail.com>
2022-03-15 17:20:41 -0500

    In accounts/views.py: *changed 'login' view to redirect to 'dashboard' insead of index upon successful auth

--- accounts/views.py
+++ accounts/views.py
@@ -50,7 +50,7 @@ def login(request):
         if user is not None:
             auth.login(request, user)
             messages.success(request, 'You are now logged in')
-            return redirect('index')
+            return redirect('dashboard') 
         else:
             messages.error(request, 'Incorrect username or password')
             return redirect('login')
