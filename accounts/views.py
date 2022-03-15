3f6c8f9e08edaa082416e2351254d60f56b25785
unknown <mperkhounkov@gmail.com>
2022-03-15 17:13:10 -0500

    Updated login function in views.py: * get username and password from field * authenticate user * if authenticated: log in, redirect to pages/index.html and display success message * if auth fails, redirect to login and display error message
    updated /pages/index.html template:
    * alerts are now displayed right below the nav bar 7 above main content
    
    updated /accounts/dashboard.html template:
    * include _alerts.html template to the top of block content
    * copy paste html from static site template

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib import messages #, auth
+from django.contrib import messages, auth
 from django.contrib.auth.models import User
 # Create your views here.
 
@@ -43,9 +43,17 @@ def register(request):
 
 def login(request):
     if request.method == 'POST':
-        print('SUBMITTED LOGIN')
-        messages.error(request, 'Testing error message')
-        return redirect('login')
+        username = request.POST['username']
+        password = request.POST['password']
+        
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            messages.success(request, 'You are now logged in')
+            return redirect('index')
+        else:
+            messages.error(request, 'Incorrect username or password')
+            return redirect('login')
 
     else:
         return render(request, 'accounts/login.html')
