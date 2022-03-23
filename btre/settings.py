bdd976eb7cfdc786c67bf516c7abe1646b59af2e
unknown <mperkhounkov@gmail.com>
2022-03-23 13:00:58 -0500

    Enable Debug for post prod development

--- btre/settings.py
+++ btre/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-k7$u=)htf-a)8r#e#^o(y+y_#sx$7z)7k+#*!*!bf=0fuso0qf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
